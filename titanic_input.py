a98a8339927abf17bafb8c4358840c6e8a56b78b
Reet Awwsum <reetawwsum@yahoo.com>
2016-06-19 01:37:39 +0530

    Processing more features

--- titanic_input.py
+++ titanic_input.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import csv
 import numpy as np
 from sklearn import preprocessing
@@ -6,28 +7,69 @@ file_path = 'dataset/'
 train_file = 'train.csv'
 test_file = 'test.csv'
 
-data = []
-target = []
-raw_data = []
+def read_train_file():
+	raw_data = []
+	target = []
 
-with open(file_path + train_file, 'rb') as f:
-	csv_reader = csv.reader(f, delimiter=',')
+	with open(file_path + train_file, 'rb') as f:
+		csv_reader = csv.reader(f, delimiter=',')
 
-	# Skipping first line
-	csv_reader.next()
+		# Skipping first line
+		csv_reader.next()
 
-	for row in csv_reader:
-		target.append(row[1])
-		raw_data.append(row[2:])
+		for row in csv_reader:
+			target.append(row[1])
+			raw_data.append(row[2:])
 
-	raw_data = np.array(raw_data)
+		raw_data = np.array(raw_data)
+		target = np.array(target).astype(int)
 
-# Adding passenger class to the feature list
-data = np.reshape(raw_data[:, 0], (-1, 1)).astype(int)
+	return raw_data, target
 
-le = preprocessing.LabelEncoder()
-le.fit(raw_data[:, 2])
+def read_test_file():
+	raw_data = []
 
-# Adding sex to the feature list
-data = np.append(data, np.reshape(le.transform(raw_data[:, 2]), (-1, 1)), 1)
+	with open(file_path + test_file, 'rb') as f:
+		csv_reader = csv.reader(f, delimiter=',')
+
+		# Skipping first line
+		csv_reader.next()
+
+		for row in csv_reader:
+			raw_data.append(row[1:])
+
+		raw_data = np.array(raw_data)
+
+	return raw_data
+
+def process_raw_data(raw_data):
+	data = []
+
+	# Adding passenger class to the feature list
+	data = np.reshape(raw_data[:, 0], (-1, 1)).astype(int)
+
+	# Adding sex to the feature list
+	le = preprocessing.LabelEncoder()
+	le.fit(raw_data[:, 2])
+
+	data = np.append(data, np.reshape(le.transform(raw_data[:, 2]), (-1, 1)), 1)
+
+	# Adding Sibsp to the feature list
+	data = np.append(data, np.reshape(raw_data[:, 4], (-1, 1)).astype(int), 1)
+
+	# Adding Parch to the feature list
+	data = np.append(data, np.reshape(raw_data[:, 5], (-1, 1)).astype(int), 1)
+
+	# Adding fare to the feature list
+	raw_data[((raw_data[:, 7] == '0') | (raw_data[:, 7] == '')) & (raw_data[:, 0] == '1'), 7] = '86.15'
+	raw_data[((raw_data[:, 7] == '0') | (raw_data[:, 7] == '')) & (raw_data[:, 0] == '2'), 7] = '21.36'
+	raw_data[((raw_data[:, 7] == '0') | (raw_data[:, 7] == '')) & (raw_data[:, 0] == '3'), 7] = '21.36'
+
+	data = np.append(data, np.reshape(raw_data[:, 7], (-1, 1)).astype(float), 1)
+
+	return data
+
+raw_data, target = read_train_file()
+
+data = process_raw_data(raw_data)
 
